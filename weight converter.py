fef403b114b1a2eb83790b23c79cfd4d767a972c
SHZ800 <123068625+SHZ800@users.noreply.github.com>
2023-02-07 10:26:28 +0100

    Add files via upload

---
+++ weight converter.py
@@ -0,0 +1,10 @@
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "k":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 2.20
+    print("Weight in Kgs: " + str(converted))
+
+
